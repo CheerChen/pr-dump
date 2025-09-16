7b7d7924210ab2df1da7812ab97eaed475dea7c8
cheerchen <cheerchen37@gmail.com>
2025-09-16 18:15:46 +0900

    fix(build): correct sha256 checksum for v0.1.0 release

--- homebrew/pr-dump.rb
+++ homebrew/pr-dump.rb
@@ -3,7 +3,7 @@ class PrDump < Formula
   desc "Dump GitHub PR context (metadata, comments, diffs) for LLM review"
   homepage "https://github.com/CheerChen/pr-dump"
   url "https://github.com/CheerChen/pr-dump/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_AFTER_RELEASE"
+  sha256 "0562fda92aff51219c373f2aff2ae0c0c27387c1b54b4934baa39d287b177883"
   license "MIT"
 
   depends_on "gh"
